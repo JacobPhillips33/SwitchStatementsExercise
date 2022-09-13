470f6c45bde22cf095fddb13fe40f29593fa74b3
Jacob Phillips <jakephillips.wv@gmail.com>
2022-09-13 17:05:07 -0500

    Add project files.

---
+++ SwitchStatementsExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("What is your favorite subject in school?");
+var favSubject = Console.ReadLine();
+
+switch (favSubject)
+{
+    case "Math":
+        Console.WriteLine("Math is my favorite too!");
+        break;
+    case "English":
+        Console.WriteLine("English is very important.");
+        break;
+    case "Science":
+        Console.WriteLine("Science is cool.");
+        break;
+    case "History":
+        Console.WriteLine("History is facinating.");
+        break;
+    case "PE":
+        Console.WriteLine("PE is a popular choice for many.");
+        break;
+    default:
+        Console.WriteLine($"{favSubject} is a unique choice.");
+        break;
+}
